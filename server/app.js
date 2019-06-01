700f422b36c67f8b272252ea5894aebe948b3853
azure desktop <aden7x@gmail.com>
2019-06-01 14:30:25 -0700

    Move express catchall for react rootpage to app.js

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(express.json());
 app.use(exampleRouter);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 module.exports = app;
--- server/routers/example.js
+++ server/routers/example.js
@@ -5,8 +5,4 @@ router.get('/api/example', (req, res) => {
     res.send({example: "example state item"});
 });
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
 module.exports = router;
